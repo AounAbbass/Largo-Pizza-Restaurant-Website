690322ef84ac363f95a982d1fb01727597f055eb
AounAbbass <aounabbass1998@gmail.com>
2024-05-22 18:15:55 +0500

    Routes Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar.jsx'
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MenuCategory from "./pages/MenuCategory.jsx";
+import Products from "./pages/Products.jsx";
+import LoginSignup from "./pages/LoginSignup.jsx";
+import Menu from "./pages/Menu.jsx";
+import Contact from "./pages/Contact.jsx";
+import Cart from "./pages/Cart.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/deals" element={<MenuCategory category="deals" />} />
+        <Route path="/products" element={<Products />}>
+          <Route path=":productId" element={<Products />} />
+        </Route>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/login" element={<LoginSignup />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 
 export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom';
 
 const Navbar= () => {
 
@@ -15,15 +16,14 @@ const [menu, setMenu] = useState("menu");
         <p>LargoPizza</p>
       </div>
       <ul className='nav-menu'>
-        <li onClick={()=>{setMenu("menu")}}>Menu{menu==="menu"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("deals")}}>Deals{menu==="deals"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("contact")}}>Contact Us{menu==="contact"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("account")}}>My Account{menu==="account"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("menu")}}><Link style={{textDecoration: 'none'}} to='/'>Menu</Link>{menu==="menu"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("deals")}}><Link style={{textDecoration: 'none'}}to='/deals'>Deals</Link>{menu==="deals"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("contact")}}><Link style={{textDecoration: 'none'}}to='/contact'>Contact Us</Link>{menu==="contact"?<hr/>:<></>}</li>
       </ul>
 
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt='' />
+        <Link to='/login'><button>Login</button></Link>
+        <Link to='/cart'><img src={cart_icon} alt='' /></Link>
         <div className='nav-cart-count'>0</div>
       </div>
     </div>
